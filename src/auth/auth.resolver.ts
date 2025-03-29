f056f7b4989946cf5b4b25c4083f8bf6a79117dd
Elvis Inoa <einoa04@gmail.com>
2025-03-28 21:32:51 -0400

    start with auth

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -5,15 +5,18 @@ import { AuthService } from './auth.service';
 export class AuthResolver {
   constructor(private readonly authService: AuthService) { }
 
-  @Mutation(() => null, { name: 'signUp' })
-  signUp(): Promise<void> {
-    return this.authService.signUp();
+  @Mutation(() => null, { name: 'signup' })
+  signup(): Promise<void> {
+    // return this.authService.signUp();
   }
 
-  @Mutation(() => null, { name: 'login' })
-  login(): Promise<void> {
-    return this.authService.login();
-  }
+  // @Mutation(() => null, { name: 'login' })
+  // login(): Promise<void> {
+  //   // return this.authService.login();
+  // }
 
-  @Query(???, { name: 'revalidate' })
+  // @Query(???, { name: 'revalidate' })
+  // async revalidateToken() {
+  //   // return this.authService.revalidateToken();
+  // }
 }
---
+++ src/auth/dto/inputs/signup.input.ts
@@ -0,0 +1,20 @@
+import { Field, InputType } from '@nestjs/graphql';
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
+@InputType()
+export class SignupInput {
+  @Field(() => String)
+  @IsEmail()
+  @IsString()
+  email: string;
+
+  @Field(() => String)
+  @IsString()
+  @IsNotEmpty()
+  fullName: string;
+
+  @Field(() => String)
+  @IsString()
+  @MinLength(6)
+  password: string;
+}
